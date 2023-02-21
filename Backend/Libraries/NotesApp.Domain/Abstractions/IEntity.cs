9b4dd1a700ccd8b8c3c4d79b560add7483884f0a
Igor Berendakov <igorberendakov@gmail.com>
2023-02-21 13:14:15 +0700

    Добавлены описания сущностей и интерфейсов, исправлен путь файлов

--- Backend/Libraries/NotesApp.Domain/Abstractions/IEntity.cs
+++ Backend/Libraries/NotesApp.Domain/Abstractions/IEntity.cs
@@ -1,7 +1,13 @@
 ﻿namespace NotesApp.Domain.Abstractions
 {
+    /// <summary>
+    /// Абстракция сущности.
+    /// </summary>
     public interface IEntity
     {
+        /// <summary>
+        /// Идентификатор сущности.
+        /// </summary>
         Guid Id { get; set; }
     }
 }
