2192557bdde66b3bf2114cd1e69d8763a966a22f
Igor Berendakov <igorberendakov@gmail.com>
2023-02-21 12:27:43 +0700

    Добавлен обобщенный репозиторий.

--- Backend/Libraries/NotesApp.Domain/Abstractions/IRepository.cs
+++ Backend/Libraries/NotesApp.Domain/Abstractions/IRepository.cs
@@ -3,8 +3,8 @@
     public interface IRepository<TEntity> where TEntity : class, IEntity
     {
         IQueryable<TEntity> GetQueryable();
-        IEnumerable<TEntity> GetAll();
-        Task<TEntity?> GetByIdAsync(Guid id);
+        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
         Task<Guid> CreateAsync(TEntity entity, CancellationToken cancellationToken = default);
         Task<bool> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
         Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default);
