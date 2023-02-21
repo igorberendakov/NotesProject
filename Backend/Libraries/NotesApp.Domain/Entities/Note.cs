b6e18e559f617a39ecd38919dda0cd58121f762a
Igor Berendakov <igorberendakov@gmail.com>
2023-02-21 09:03:52 +0700

    Добавлены абстракции и сущности в domain слой.

--- Backend/Libraries/NotesApp.Domain/Entities/Note.cs
+++ Backend/Libraries/NotesApp.Domain/Entities/Note.cs
@@ -1,7 +1,20 @@
-﻿namespace NotesApp.Domain.Entities
-{
-    public class Note
-    {
+﻿using NotesApp.Domain.Abstractions;
 
+namespace NotesApp.Domain.Entities
+{
+    public class Note : IEntity
+    {
+        /// <summary>
+        /// Идентификатор заметки.
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Заголовок заметки.
+        /// </summary>
+        public string Title { get; set; } = null!;
+        /// <summary>
+        /// Текст заметки.
+        /// </summary>
+        public string Text { get; set; } = null!;
     }
 }
---
+++ Backend/Libraries/NotesApp.Domain/Entities/Notification.cs
@@ -0,0 +1,24 @@
+﻿using NotesApp.Domain.Abstractions;
+
+namespace NotesApp.Domain.Entities
+{
+    internal class Notification : IEntity
+    {
+        /// <summary>
+        /// Идентификатор напоминания.
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Идентификатор заметки
+        /// </summary>
+        public Guid NoteId { get; set; }
+        /// <summary>
+        /// Привязка ко времени.
+        /// </summary>
+        public DateTime? TimeBinding { get; set; }
+        /// <summary>
+        /// Заметка.
+        /// </summary>
+        public virtual Note? Note { get; set; }
+    }
+}
